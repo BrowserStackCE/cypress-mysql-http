91cc86c4dc95a2166adf969b43c00ade9118db81
tej.ozymandias <tej.ozymandias@gmail.com>
2023-04-27 19:34:36 +0530

    added final tests

--- cypress.config.js
+++ cypress.config.js
@@ -9,8 +9,8 @@ module.exports = defineConfig({
     "video": false,
     "env":{
       "db": {
-        "host": "0.0.0.0",
-        "port":"8081",
+        "host": "127.0.0.1",
+        "port":"3306",
         "user": "root",
         "password": "Browserstack@123",
         "database":"Demo1"
--- cypress/e2e/MyTests/mySpec.cy.js
+++ cypress/e2e/MyTests/mySpec.cy.js
@@ -1,19 +1,58 @@
 
 describe("DB Testing", function(){
-  it.only("Check Assertion", function(){
-    cy.task("queryDb",`SELECT * FROM Demo1.Persons WHERE LastName='Kumar'`).then(count => {
-      expect(count).to.have.lengthOf(1);
+  it.only("Select all rows from Persons Table", function(){
+    cy.request({
+      url: `http://127.0.0.1:5000/query`,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: {
+        // Selecting all values from Persons Table where Last Name is Kumar
+        mysqlQuery: "SELECT * FROM Demo1.Persons WHERE LastName='Kumar'",
+      },
+    }).then((response) =>{
+      // Expecting Response to contain the Last Name of Kumar
+        expect(response.body.LastName == "Kumar")
+        console.log("Showing Result of the Lastname "+response.body.LastName)
     });
+  })
+    
+
+  it.only("Insert Row into Database", function(){
+
+    cy.request({
+      url: `http://127.0.0.1:5000/query`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: {
+        // Insert a new row into Persons Table
+        mysqlQuery: "INSERT INTO Persons (PersonID) VALUES('007')"
+      },
+    })
+    .then((response) =>{
+       // Expecting a single row to be affected
+      expect(response.body.affectedRows ==1);
+      console.log("affected rows= "+response.body.affectedRows);
+      // console.log(response.status);
+    });
+
   });
 
 
-  it.only("InsertIntoDb", function(){
-    cy.task("queryDb",`INSERT INTO Persons (LastName) VALUES('Gates')`)
-    // .then((result) =>{
-    //   expect(result.affectedRows).to.equal(2);
-    // });
+  // it.only("Check Assertion", function(){
+  //   cy.task("queryDb",`SELECT * FROM Demo1.Persons WHERE LastName='Kumar'`).then(count => {
+  //     expect(count).to.have.lengthOf(1);
+  //   });
+  // });
+      // cy.task("queryDb",`INSERT INTO Persons (LastName) VALUES('Agent')`)
 
-  });
+
+  
+
+ 
 
 });
 
